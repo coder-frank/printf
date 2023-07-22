d0e9a4b0949b08c9bb71181dcfa13b0e8edd433d
Apah Benson <junerob4shot@gmail.com>
2023-07-22 18:01:20 +0100

    refactored code added

--- _printf.c
+++ _printf.c
@@ -2,19 +2,52 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+
 /**
- * _printf - prints any character
- * @format: The format
- * Return: The size of the string
+* print_char - Print a character
+*/
+void print_char(va_list ap)
+{
+	char word = va_arg(ap, int);
+	putchar(word);
+}
+
+/**
+* print_string - Print a string
+*/
+void print_string(va_list ap)
+{
+	char *string = va_arg(ap, char *);
+	printf("%s", string);
+}
+
+/**
+* print_integer - Print an integer
+*/
+void print_integer(va_list ap)
+{
+	int number = va_arg(ap, int);
+	printf("%d", number);
+}
+
+/**
+* print_octal - Print an octal number
+*/
+void print_octal(va_list ap)
+{
+	int octal = va_arg(ap, int);
+	printf("%i", octal);
+}
+
+/**
+* _printf - Prints formatted output
 */
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	char word;
-	char *string;
-	int number, octal, size, j;
-	va_start(ap, format);
+	int size = 0, j;
 
+	va_start(ap, format);
 	if (format == NULL)
 		return (-1);
 
@@ -23,41 +56,38 @@ int _printf(const char *format, ...)
 	{
 		if (format[j] == '\n')
 			size--;
-		if (format[j] == '%')
-		{
-			j++;
-			switch (format[j])
+			if (format[j] == '%')
 			{
-				case 'c':
-					word = va_arg(ap, int);
-					printf("%c", word);
-					break;
-				case 's':
-					string = va_arg(ap, char *);
-					printf("%s", string);
-					break;
-				case 'd':
-					number = va_arg(ap, int);
-					printf("%d", number);
-					break;
-				case 'i':
-					octal = va_arg(ap, int);
-					printf("%i", octal);
-					break;
-				case '%':
-					va_arg(ap, char *);
-					printf("%%");
-					break;
-
-				default:
-					putchar('%');
-					putchar(format[j]);
-					break;
+				j++;
+				switch (format[j])
+				{
+					case 'c':
+						print_char(ap);
+						break;
+					case 's':
+						print_string(ap);
+						break;
+					case 'd':
+						print_integer(ap);
+						break;
+					case 'i':
+						print_octal(ap);
+						break;
+					case '%':
+						putchar('%');
+						break;
+					default:
+						putchar('%');
+						putchar(format[j]);
+						break;
+				}
 			}
-		} else
-		{
-			putchar(format[j]);
-		}
+			else
+			{
+				putchar(format[j]);
+			}
 	}
+	va_end(ap);
 	return (size);
 }
+
