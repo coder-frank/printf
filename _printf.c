ee8580ed70673979fcd19e9c39b7f1f19d96714d
Apah Benson <junerob4shot@gmail.com>
2023-07-22 19:24:56 +0100

    another commit

--- _printf.c
+++ _printf.c
@@ -4,62 +4,61 @@
 #include <string.h>
 
 /**
- * _printf - prints any character
- * @format: The format
- * Return: The size of the string
- */
+* _printf - prints any character
+* @format: The format
+* Return: The size of the string
+*/
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    char word;
-    char *string;
-    int number, octal, size, j;
-    va_start(ap, format);
+	va_list ap;
+	char word;
+	char *string;
+	int number, octal, size, j;
+	va_start(ap, format);
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    size = strlen(format);
-    for (j = 0; j < size; j++)
-    {
-        if (format[j] == '\n')
-            size--;
-        if (format[j] == '%')
-        {
-            j++;
-            switch (format[j])
-            {
-                case 'c':
-                    word = va_arg(ap, int);
-                    printf("%c", word);
-                    break;
-                case 's':
-                    string = va_arg(ap, char *);
-                    printf("%s", string);
-                    break;
-                case 'd':
-                    number = va_arg(ap, int);
-                    printf("%d", number);
-                    break;
-                case 'i':
-                    octal = va_arg(ap, int);
-                    printf("%i", octal);
-                    break;
-                case '%':
-                    va_arg(ap, char *);
-                    printf("%%");
-                    break;
-
-                default:
-                    putchar('%');
-                    putchar(format[j]);
-                    break;
-            }
-        }
-        else
-        {
-            putchar(format[j]);
-        }
-    }
-    return (size);
+	size = strlen(format);
+	for (j = 0; j < size; j++)
+	{
+		if (format[j] == '\n')
+			size--;
+		if (format[j] == '%')
+		{
+			j++;
+			switch (format[j])
+			{
+				case 'c':
+					word = va_arg(ap, int);
+					printf("%c", word);
+					break;
+				case 's':
+					string = va_arg(ap, char *);
+					printf("%s", string);
+					break;
+				case 'd':
+					number = va_arg(ap, int);
+					printf("%d", number);
+					break;
+				case 'i':
+					octal = va_arg(ap, int);
+					printf("%i", octal);
+					break;
+				case '%':
+					va_arg(ap, char *);
+					printf("%%");
+					break;
+				default:
+					putchar('%');
+					putchar(format[j]);
+					break;
+			}
+		}
+		else
+		{
+			putchar(format[j]);
+		}
+	}
+	return (size);
 }
